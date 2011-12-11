fedf509f9c2a05d620face38e4346fcf194801ba
xdite <xuite.joke@gmail.com>
2011-12-11 13:13:02 +0800

    add sidebar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   
   def welcome
+    unless current_user.blank?
+      redirect_to posts_path 
+      drop_breadcrumb("Hello")
+      return
+    end
   end
-  
+
 end
